27b8ff3591f1462ecbc7d9f01e91a23e69bae172
Janos Perge <janos.perge@gmail.com>
2024-01-24 22:16:15 +0100

    add CourseService error scenario

--- test/service/CourseService.test.ts
+++ test/service/CourseService.test.ts
@@ -6,6 +6,7 @@ import { CourseRepository } from '../../src/repository/CourseRepository'
 import { Course } from '../../src/models/Course'
 import { Student } from '../../src/models/Student'
 import { CourseFixture, StudentFixture } from '../fixtures'
+import { NotFoundError } from '../../src/exceptions/NotFoundError'
 
 const mockedCourseRepository = mock<CourseRepository>()
 const mockedPaymentService = mock<PaymentService>()
@@ -26,69 +27,99 @@ describe('CourseService tests', () => {
     )
   })
 
-  it('should add Course', async () => {
-    // Arrange
-    const course = new Course('Clean Code', new Date('2023-12-06'), 6, 69900)
-    mockedCourseRepository.AddCourse.mockImplementation()
+  describe('Happy paths', () => {
+    it('should add Course', async () => {
+      // Arrange
+      const course = new Course('Clean Code', new Date('2023-12-06'), 6, 69900)
+      mockedCourseRepository.AddCourse.mockImplementation()
 
-    // Act
-    courseService.AddCourse(course)
+      // Act
+      courseService.AddCourse(course)
 
-    expect(mockedCourseRepository.AddCourse).toHaveBeenCalledTimes(1)
-    expect(mockedCourseRepository.AddCourse).toHaveBeenCalledWith(course)
+      expect(mockedCourseRepository.AddCourse).toHaveBeenCalledTimes(1)
+      expect(mockedCourseRepository.AddCourse).toHaveBeenCalledWith(course)
+    })
+
+    it('should get all courses', async () => {
+      // Arrange
+      mockedCourseRepository.GetCourses.mockImplementation()
+
+      // Act
+      courseService.GetCourses()
+
+      // Assert
+      expect(mockedCourseRepository.GetCourses).toHaveBeenCalledTimes(1)
+      expect(mockedCourseRepository.GetCourses).toHaveBeenCalledWith()
+    })
+
+    it('should get Course Statistic', async () => {
+      // Arrange
+      const courseName = 'Clean Code'
+      mockedCourseRepository.GetCourseStatistics.mockImplementation()
+
+      // Act
+      await courseService.GetCourseStatistics(courseName)
+
+      // Assert
+      expect(mockedCourseRepository.GetCourseStatistics).toHaveBeenCalledTimes(
+        1
+      )
+      expect(mockedCourseRepository.GetCourseStatistics).toHaveBeenCalledWith(
+        courseName
+      )
+    })
+
+    it('should add Student to Course', async () => {
+      // Arrange
+      const student = StudentFixture
+      const course = CourseFixture
+      const courseName = 'Clean Code'
+
+      mockedCourseRepository.GetCourseByName.mockResolvedValue(course)
+      mockedPaymentService.GetIsOrderPayed.mockResolvedValue(true)
+      mockedNotificationService.SendNotifications.mockImplementation()
+
+      // Act
+      await courseService.AddStudentToCourse(student, courseName)
+
+      // Assert
+      expect(mockedCourseRepository.GetCourseByName).toHaveBeenCalledTimes(1)
+      expect(mockedCourseRepository.GetCourseByName).toHaveBeenCalledWith(
+        courseName
+      )
+      expect(mockedPaymentService.GetIsOrderPayed).toHaveBeenCalledTimes(1)
+      expect(mockedPaymentService.GetIsOrderPayed).toHaveBeenCalledWith(student)
+      expect(mockedNotificationService.SendNotifications).toHaveBeenCalledTimes(
+        1
+      )
+      expect(mockedNotificationService.SendNotifications).toHaveBeenCalledWith(
+        'John Doe student was added to course.'
+      )
+      expect(course.GetStudents()).toStrictEqual([student])
+    })
   })
 
-  it('should get all courses', async () => {
-    // Arrange
-    mockedCourseRepository.GetCourses.mockImplementation()
+  describe('Error paths', () => {
+    it('should throw error via add Student to Course because Course not found', async () => {
+      // Arrange
+      const student = StudentFixture
+      const course = CourseFixture
+      const courseName = 'Clean Code'
+      const errorMessage = 'Course not found'
+      const expectedError = new NotFoundError(errorMessage)
 
-    // Act
-    courseService.GetCourses()
+      mockedCourseRepository.GetCourseByName.mockRejectedValue(() => {
+        throw expectedError
+      })
 
-    // Assert
-    expect(mockedCourseRepository.GetCourses).toHaveBeenCalledTimes(1)
-    expect(mockedCourseRepository.GetCourses).toHaveBeenCalledWith()
-  })
-
-  it('should get Course Statistic', async () => {
-    // Arrange
-    const courseName = 'Clean Code'
-    mockedCourseRepository.GetCourseStatistics.mockImplementation()
-
-    // Act
-    await courseService.GetCourseStatistics(courseName)
-
-    // Assert
-    expect(mockedCourseRepository.GetCourseStatistics).toHaveBeenCalledTimes(1)
-    expect(mockedCourseRepository.GetCourseStatistics).toHaveBeenCalledWith(
-      courseName
-    )
-  })
-
-  it('should add Student to Course', async () => {
-    // Arrange
-    const student = StudentFixture
-    const course = CourseFixture
-    const courseName = 'Clean Code'
-
-    mockedCourseRepository.GetCourseByName.mockResolvedValue(course)
-    mockedPaymentService.GetIsOrderPayed.mockResolvedValue(true)
-    mockedNotificationService.SendNotifications.mockImplementation()
-
-    // Act
-    await courseService.AddStudentToCourse(student, courseName)
-
-    // Assert
-    expect(mockedCourseRepository.GetCourseByName).toHaveBeenCalledTimes(1)
-    expect(mockedCourseRepository.GetCourseByName).toHaveBeenCalledWith(
-      courseName
-    )
-    expect(mockedPaymentService.GetIsOrderPayed).toHaveBeenCalledTimes(1)
-    expect(mockedPaymentService.GetIsOrderPayed).toHaveBeenCalledWith(student)
-    expect(mockedNotificationService.SendNotifications).toHaveBeenCalledTimes(1)
-    expect(mockedNotificationService.SendNotifications).toHaveBeenCalledWith(
-      'John Doe student was added to course.'
-    )
-    expect(course.GetStudents()).toStrictEqual([student])
+      // Assert
+      expect(() =>
+        courseService.AddStudentToCourse(student, courseName)
+      ).rejects.toThrow(expectedError)
+      expect(mockedCourseRepository.GetCourseByName).toHaveBeenCalledTimes(1)
+      expect(mockedCourseRepository.GetCourseByName).toHaveBeenCalledWith(
+        courseName
+      )
+    })
   })
 })
